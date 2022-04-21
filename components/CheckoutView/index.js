a14ccbf5938e5cd37e3572a5c7e344e0a4b52b24
princetp634 <dfdf>
2022-04-21 15:58:19 +0530

    added payment flow & api

--- components/CheckoutView/index.js
+++ components/CheckoutView/index.js
@@ -1,14 +1,44 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
+import Script from 'next/script'
 
-import { getPlanDetail } from '@/actions/index.js';
+import { validateEmail } from '@/helpers/utils.js';
+import { handleCheckout, getPlanDetail } from '@/actions/index.js';
+import CustomToaster from '@/components/CustomToaster';
 
 import CheckoutForm from './CheckoutForm.js';
 import { container } from './style.js';
 
 const ContactForm = ()=>{
     const router = useRouter();
+    const submitRef = useRef();
     const [planDetails, setPlanDetails] = useState({});
+    const [toasterInfo, setToasterInfo] = useState({
+        isVisible: false,
+        isError: false,
+        isSuccess: false,
+        msg: ''
+    })
+    const { isVisible, isError, isSuccess, msg } = toasterInfo;
+    const { title='', price='' } = planDetails||{}
+
+
+    const showToaster = (isSucess=false, errorMsg='')=>{
+        setToasterInfo({
+            isVisible: true,
+            isError: !isSucess,
+            isSuccess: isSucess,
+            msg: errorMsg
+        })
+        setTimeout(() => {
+            setToasterInfo({
+                isVisible: false,
+                isError: false,
+                isSuccess: false,
+                msg:''
+            })
+        }, 2000);
+    }
 
     useEffect(()=>{
         let id;
@@ -20,8 +50,6 @@ const ContactForm = ()=>{
         }
         if(id){
             getPlanDetail({id}, (resp)=>{
-
-                console.log(resp);
                 if(resp){
                     setPlanDetails(resp);
                 }
@@ -30,18 +58,94 @@ const ContactForm = ()=>{
             showToaster(false, 'Please Select Plan first');
             setTimeout(() => {
                 router.push('/plans');
-            }, 3000);
+            }, 1000);
         }
     },[])
 
-    const { title='', price='' } = planDetails||{}
+    const handleClick = ()=>{
+        submitRef.current.submitData();
+    }
 
+    const redirectToRazorPay = (orderId, userInfo)=>{
+        const { email, first_name, last_name, amount, phone_number } = userInfo;
+        const options = {
+            "key": "rzp_live_SHoE722sQX5Zzg", // Enter the Key ID generated from the Dashboard
+            amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+            "currency": "INR",
+            "name": "Acme Corp",
+            "description": "Test Transaction",
+            "image": "https://example.com/your_logo",
+            "order_id": orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+            "handler": function (response){
+            },
+            "prefill": {
+                "name": `${first_name} ${last_name}`,
+                email,
+                "contact": phone_number
+            },
+            "notes": {
+                "address": "Razorpay Corporate Office"
+            },
+            "theme": {
+                "color": "#3399cc"
+            }
+        };
+        var rzp1 = new Razorpay(options);
+        rzp1.on('payment.failed', function (response){
+            showToaster(false, response.error.reason);
+        });
+        rzp1.open();
+    }
+
+    const handlePlaceOrder = (val)=>{
+        const { fName, lName, email, phoneNo } = val;
+        if(!fName){
+            showToaster(false, 'Please enter valid First name')
+            return;
+        }
+        if(!email){
+            showToaster(false, 'Please enter valid email')
+            return;
+        }
+        if(!validateEmail(email)){
+            showToaster(false, 'Please enter valid email')
+            return;
+        }
+        if(!phoneNo){
+            showToaster(false, 'Please enter valid phone number')
+            return;
+        }
+        const postParams = {
+            first_name: fName,
+            last_name: lName,
+            phone_number: phoneNo,
+            email,
+            amount: price,
+            currency: "INR"
+        }
+        handleCheckout(postParams).then((resp)=>{
+            if(resp && resp.order_id){
+                redirectToRazorPay(resp.order_id, postParams);
+                showToaster(true, 'Details Saved Successfully, we will get back to you.');
+            }else{
+                console.error('failed to create checkout lead', resp);
+                showToaster(false, 'Failed to save details, Please try again later');
+            }
+        }).catch((e)=>{
+            console.error(e);
+            showToaster(false, 'Failed to save details, Please try again later');
+        })
+    }
 
     return(
         <div className={container}>
+            {
+                isVisible?<CustomToaster isVisible={isVisible} isError={isError} isSuccess={isSuccess} msg={msg}/>:null
+            }
+            <Script src="https://checkout.razorpay.com/v1/checkout.js" />
             <div className='row'>
                 <div className='col-md-6 col-12'>
-                    <CheckoutForm/>
+                    <CheckoutForm handlePlaceOrder={handlePlaceOrder} ref={submitRef}/>
                 </div>
                 <div className='col-md-6 col-12'>
                     <div className='orderBox'>
@@ -73,7 +177,7 @@ const ContactForm = ()=>{
                             <li class="">
                                 <div className='payRadio'>
 
-                                    <input type="radio" />
+                                    <input type="radio" checked/>
 
                                     <label for="">
                                         Credit Card/Debit Card/NetBanking <img src="https://cdn.razorpay.com/static/assets/logo/payment.svg" alt="Credit Card/Debit Card/NetBanking" />
@@ -89,7 +193,7 @@ const ContactForm = ()=>{
 
                             <p>Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our
                                 <a href="https://getkeel.com/privacy-policy/" class="" target="_blank">privacy policy</a>.</p>
-                            <button type="submit" class="button alt" name="" id="place_order" value="Place order" data-value="Place order">Place order</button>
+                            <button type="submit" class="button alt" name="" id="place_order" value="Place order" data-value="Place order" onClick={handleClick}>Place order</button>
 
                         </div>
                     </div>
